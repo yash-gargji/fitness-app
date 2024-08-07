bd6998a4eb94b80fa78114ecaffa5e02d8167880
Yash Garg <yash.gargji2004@gmail.com>
2024-08-07 18:41:00 +0530

    Some Changes

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'a2364f57f8msha65d4a40f8e536dp10324bjsnee0c08ea1dd1',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
   },
 };
 
